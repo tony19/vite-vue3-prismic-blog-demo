d782003e1d3d57da756c4dd06d0ce88ab1bbe558
Tony Trinh <tony19@gmail.com>
2021-09-05 17:17:32 -0500

    restore shims-prismic.d.ts

--- src/shims-prismic.d.ts
+++ src/shims-prismic.d.ts
@@ -1,21 +1,22 @@
-// import Vue from 'vue'
-// import { RichText, Link, HTMLSerializer } from 'prismic-dom'
-// import { Client } from '@prismicio/client'
+import Vue from 'vue'
+import { RichText, Link, HTMLSerializer } from 'prismic-dom'
+import { default as prismicClient } from '@prismicio/client'
 
-// declare module 'vue/types/vue' {
-//   interface Vue {
-//     $prismic: Omit<typeof prismicClient, 'client'> & {
-//       // properties
-//       endpoint: string
-//       linkResolver: (doc: any) => string
-//       htmlSerializer: typeof HTMLSerializer
-//       client: Client
+declare module 'vue/types/vue' {
+  interface Vue {
+    $prismic: Omit<typeof prismicClient, 'client'> & {
+      // properties
+      endpoint: string
+      linkResolver: (doc: any) => string
+      htmlSerializer: typeof HTMLSerializer
+      client: ReturnType<typeof prismicClient.client>
 
-//       // methods
-//       asText: typeof RichText['asText']
-//       asHtml: typeof RichText['asHtml']
-//       asLink: typeof Link['url']
-//       asDate: (date: string | number | Date) => Date
-//     }
-//   }
-// }
+      // methods
+      asText: typeof RichText['asText']
+      richTextAsPlain: typeof RichText['asText']
+      asHtml: typeof RichText['asHtml']
+      asLink: typeof Link['url']
+      asDate: (date: string | number | Date) => Date
+    }
+  }
+}
