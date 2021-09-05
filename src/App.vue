c05343f6985517a19c6c87c6b21fa0f6c0c5db2f
Tony Trinh <tony19@gmail.com>
2021-09-05 18:04:26 -0500

    use Vue 2 in App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from '@vue/composition-api'
 import FooterPrismic from '@/components/FooterPrismic.vue'
 
 export default defineComponent({
